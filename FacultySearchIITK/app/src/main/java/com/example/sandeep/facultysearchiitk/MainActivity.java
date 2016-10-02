af47391f46c14b300091381e844381f4dea89581
Sandeep Kumar <Sandeep Kumar>
2016-10-02 12:21:03 +0530

    random commit

--- FacultySearchIITK/app/src/main/java/com/example/sandeep/facultysearchiitk/MainActivity.java
+++ FacultySearchIITK/app/src/main/java/com/example/sandeep/facultysearchiitk/MainActivity.java
@@ -2,6 +2,8 @@ package com.example.sandeep.facultysearchiitk;
 
 import android.content.Context;
 import android.content.Intent;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -22,7 +24,10 @@ public class MainActivity extends AppCompatActivity {
     RadioButton nm,usn;
     EditText finalresult;
     Button btn=null;
-    
+    String Name=null,Username=null,Rinterest=null,Sex=null,Department=null,designation=null;
+    int flag=0;
+    String dept=null;
+
 
 
 
@@ -63,6 +68,8 @@ public class MainActivity extends AppCompatActivity {
             public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
                 if(count>=3){
                     Toast.makeText(getBaseContext(),parent.getItemAtPosition(position)+" selected", Toast.LENGTH_LONG).show();
+                    dept = Dept.getSelectedItem().toString();
+
                 }
                 count++;
             }
@@ -98,21 +105,43 @@ public class MainActivity extends AppCompatActivity {
         boolean checked=((RadioButton) view).isChecked();
         if(view.getId()==nm.getId()&&checked){
         finalresult.setHint("Please enter the Name");
+        Name=finalresult.getText().toString();
+        flag=1;
         }
         else if(view.getId()==usn.getId()&&checked){
             finalresult.setHint("Please enter the username");
+            Username=finalresult.getText().toString();
+            flag=2;
         }
         else{
             finalresult.setHint("Research Interest Please");
+            Rinterest=finalresult.getText().toString();
+            flag=3;
         }
     }
 
     public void Search(View view){
         Intent intent = new Intent(MainActivity.this , second_activity.class);
-        startActivity(intent);
-        
+
+// EditText txt= (EditText)findViewById(R.id.edit);
+////       txt.setText(dept);
+//        intent.putExtra("One",dept);
+        if(dept!=null)
+        {
+//
+        SQLiteDatabase mydatabase = openOrCreateDatabase("database/"+dept+"/",MODE_PRIVATE,null);
+        Cursor resultSet = mydatabase.rawQuery("Select * from ? WHERE lower(name) = ?",new String[]{dept.toLowerCase(), Name.toLowerCase()});
+            resultSet.moveToFirst();
+//
+//            txt.setText(resultSet.getString(1));
+//
+            intent.putExtra("One",resultSet.getString(1));
+            startActivity(intent);
+//
+//
+//
     }
 
 
-}
+}}
 
--- FacultySearchIITK/app/src/main/java/com/example/sandeep/facultysearchiitk/second_activity.java
+++ FacultySearchIITK/app/src/main/java/com/example/sandeep/facultysearchiitk/second_activity.java
@@ -2,15 +2,32 @@ package com.example.sandeep.facultysearchiitk;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.EditText;
+import android.content.Intent;
+
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.support.v7.app.AppCompatActivity;
+
+
+
+import static com.example.sandeep.facultysearchiitk.R.array.dept;
 
 /**
  * Created by Sandeep on 10/2/2016.
  */
+//String bet;
+
 
 public class second_activity extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.secondlayout);
+//        Intent intent = getIntent();
+        String bet = getIntent().getStringExtra("One");
+//
+        EditText txt= (EditText)findViewById(R.id.edit);
+       txt.setText(bet);
     }
 }
